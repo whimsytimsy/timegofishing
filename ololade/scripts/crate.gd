9d1cbeb0e60893ce02ad291aa1a6904b22cd1f93
ol01-ad3 <ololadeisanya@gmail.com>
2026-07-01 11:51:39 +0900

    finished first camera ui controls testing second

--- ololade/scripts/crate.gd
+++ ololade/scripts/crate.gd
@@ -1,11 +1,14 @@
-extends Button
+extends RigidBody2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	visible = false
+	set_collision_layer_value(2, false)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("camera_1"):
+		visible = !visible
+		set_collision_layer_value(2, visible)
--- ololade/scripts/player.gd
+++ ololade/scripts/player.gd
@@ -2,10 +2,12 @@ extends CharacterBody2D
 
 @export var speed = 30.0
 @export var jump_power = 10.0
+@export var push_force = 100.0
 
 var speed_multiplyer = 30.0
 var jump_multiplyer = -30.0
 var direction = 0
+
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -27,3 +29,16 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplyer)
 
 	move_and_slide()
+
+# Loop through all collisions that occurred during move_and_slide
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		
+		if collider.visible:
+			# Check if the collided object is a RigidBody2D
+			if collider is RigidBody2D:
+				# Apply an instantaneous kick in the direction opposite to the collision normal
+				# Multiplying by player velocity allows a faster player to push harder
+				var push_direction = -collision.get_normal()
+				collider.apply_central_impulse(push_direction * push_force)
