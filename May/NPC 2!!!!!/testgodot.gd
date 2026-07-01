0495f548358a3e17ec1545569e8aeacfc6354f13
May-Zar <mayzar126@gmail.com>
2026-07-01 14:07:21 +0900

    talking godot

--- May/NPC 2!!!!!/testgodot.gd
+++ May/NPC 2!!!!!/testgodot.gd
@@ -8,5 +8,9 @@ func _ready() -> void:
 	
 func _on_interact():
 	interactable.is_interactable = false 
+	$Dialogue.start()
 	print("the player get's a new camera")
-		
+
+
+func _on_dialogue_dialogue_finshed() -> void:
+	interactable.is_interactable = false
